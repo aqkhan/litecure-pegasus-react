da6bc9b8c901402aebd7a3114c52b2338478989a
akashiintellect <akash.iintellect@gmail.com>
2018-12-13 22:40:23 +0500

    publications detail page

--- components/publicationDetail/detailcontent/index.js
+++ components/publicationDetail/detailcontent/index.js
@@ -9,6 +9,10 @@ const detailContent = () => {
             <div className="container">
                 <div className="title-dev">
                     <p className="w-text">Severe Bursitis and Osteoarthritis, American Quarter Horse Gelding</p>
+                    <div className="row">
+                        <button className="download-btn-red"><a href="#">DOWNLOAD PDF</a></button>
+                        <button className=" full-study-btn"><a href="#">READ FULL STUDY</a></button>
+                    </div>
                     <span>Author(s): "Lisa Miller, DVM, CCRT"</span>
                 </div>
                 <div className="description-dev">
@@ -19,6 +23,33 @@ const detailContent = () => {
                     <p><em><strong>Patient Updates:&nbsp;</strong></em><strong>&nbsp;</strong>After June 2013 Rio received maintenance laser treatments once weekly to his elbow 4-8 weeks for the following year and then only on an “as needed” basis.&nbsp; He is not on any systemic medications, just a chondroprotectant supplement in his feed daily.</p>
                     <p>Video courtesy of C. Howse</p>
                     <p><iframe allow="autoplay; encrypted-media" allowfullscreen="" frameborder="0" height="600" src="https://www.youtube.com/embed/nen79QAhH88?ecver=1" width="100%"></iframe></p>
+                    <h3 className="about-author">About the authors (s)</h3>
+                    <div className="authors-details-card">
+                        <div className="card">
+                            <div className="card-body">
+                                <div className="author-img-div"></div>
+                                <div>
+                                    <p className="author-name">KHASMI YADDDA NAME,KHASMI YADDDA NAME 2,KHASMI YADDDA NAME</p>
+
+
+
+                                </div>
+                                <div>
+                                <p className="author-bio">Lorem Ipsum is simply dum
+                                    my text of the printing and typesetting industry.
+                                    Lorem Ipsum has been the industry's standard dummy text ever sin
+                                    ce the 1500s, when an unknown printer took a galley of type and scrambled
+                                    it to make a type specimen book. It has survived not only five centuries, b
+                                    ut also the leap into electronic typesetting, remaining essentially unc
+                                    hanged. It was popularised in the 1960s with the release of Letraset
+                                    sheets containing Lorem Ipsum passages, and more recently with desktop publishin
+                                    g software like Aldus PageMaker including versions of Lorem Ipsum.
+
+                                </p></div>
+
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
