adafacc29771ebe80ffa37713d2ebfbb24dd7290
saadalishah <socialsolution15@gmail.com>
2018-12-28 15:46:33 +0500

    updated

--- components/employment/employmentPage/index.js
+++ components/employment/employmentPage/index.js
@@ -2,10 +2,17 @@
  * Created by FaZi on 12/27/2018.
  */
 import React, {Component} from 'react';
-
 class Employe extends Component {
     render(){
-        return <h1>hello</h1>
+        return <section className="employees">
+            <div className="container">
+                <div className="detail-emp">
+                    <div className="description-text" dangerouslySetInnerHTML={{__html:this.props.content}}>
+
+                    </div>
+                </div>
+            </div>
+        </section>
     }
 }
 export default Employe;
--- components/employment/index.js
+++ components/employment/index.js
@@ -1,12 +1,40 @@
 import React, { Component } from "react";
 import Employe from './employmentPage/index'
+import Employmentheader from './employmentHeader/employmentheader'
+import axios from 'axios';
+import {API_PATH} from '../apiconfig';
+
 class employeesPage extends Component{
+    state = {
+        employee: null,
+        err:null
+    }
+    componentWillMount(){
+        axios.get(API_PATH +'pages')
+            .then((res)=>{
+                let temp=[];
+                res.data.pages.forEach((val)=>{
+                    if (val.type==="employment"){
+                        temp.push(val);
+                    }
+
+                });
+
+                this.setState({employee:temp})
+
+            })
+            .catch(err=>{throw err});
+    }
     render(){
-        return(
-            <div>
-              <Employe/>
-            </div>
-        )
+
+        let {employee} = this.state;
+
+            return employee&&(<div>
+
+                <Employmentheader title = {employee[0].title} leadText = {employee[0].leadText} imgUrl = {employee[0].featuredImage&&employee[0].featuredImage.url}/>
+                <Employe content ={employee[0].content}/>
+
+            </div>)
     }
 }
 
