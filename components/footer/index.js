f784d5a57cdb380ffd63a36407552fb73c6c51c9
akashiintellect <akash.iintellect@gmail.com>
2019-01-03 03:02:42 +0500

    new feed-back

--- components/footer/index.js
+++ components/footer/index.js
@@ -1,5 +1,53 @@
 import React, { Component } from "react";
+import Link from 'next/link';
 class Footer extends Component{
+    state={
+        check:false,
+        test:null
+    };
+    componentWillMount() {
+        if (typeof document !== 'undefined') {
+            this.callFbScript(document, 'script', 'facebook-jssdk');
+        }
+    }
+    // componentWillUpdate() {
+    //         this.callFbScript(document, 'script', 'facebook-jssdk');
+    // }
+    // componentDidMount() {
+    //     this.callFbScript(document, 'script', 'facebook-jssdk');
+    // }
+    // componentWillReceiveProps(nextProps, nextContext) {
+    //     let {test}=nextProps;
+    //     this.setState({test})
+    //     this.callFbScript(document, 'script', 'facebook-jssdk');
+    // }
+    // componentDidUpdate(prevProps, prevState, snapshot) {
+    //     this.callFbScript(document, 'script', 'facebook-jssdk');
+    // }
+    // shouldComponentUpdate(nextProps, nextState, nextContext) {
+    //     return true;
+    // }
+
+    callFbScript(d, s, id){
+            setTimeout(()=>{
+                let js, fjs = d.getElementsByTagName(s)[0];
+                if (d.getElementById(id)) return;
+                js = d.createElement(s); js.id = id;
+                js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.2';
+                fjs.parentNode.insertBefore(js, fjs,);
+                this.setState({check:true})
+                },100)
+    }
+
+    // callFbScript2(d, s, id){
+    //     setTimeout(()=>{
+    //         let js, fjs = d.getElementsByTagName(s)[0];
+    //         if (d.getElementById(id)) return;
+    //         js = d.createElement(s); js.id = id;
+    //         js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.2';
+    //         fjs.parentNode.insertBefore(js, fjs,);
+    //     },500)
+    // }
 
     render(){
         return(
@@ -20,31 +68,38 @@ class Footer extends Component{
                                 <div className="col-sm-2 col-2 all-width mt-5">
                                     <div className="redcolor"><p className="make-bold"><strong>LiteCure</strong></p></div>
                                     <div className="footer-menu">
-                                        <div><a href="#">About US</a></div>
-                                        <div><a href="#">Contact LiteCure</a></div>
-                                        <div><a href="#">Contact Sales</a></div>
-                                        <div><a href="#">Core Values</a></div>
-                                        <div><a href="#">Products</a></div>
-                                        <div><a href="#">Careers</a></div>
+                                        <div><Link href="/about-us"><a>About US</a></Link></div>
+                                        <div><Link href="#"><a>Contact LiteCure</a></Link></div>
+                                        <div><Link href="/support"><a>Contact Sales</a></Link></div>
+                                        <div><Link href=""><a>Core Values</a></Link></div>
+                                        <div><Link href="/products"><a>Products</a></Link></div>
+                                        <div><Link href="/employees"><a>Careers</a></Link></div>
                                     </div>
                                 </div>
                                 <div className="col-md-3 all-width mt-5">
                                     <div className="redcolor"><p className="make-bold"><strong>Facebook</strong></p></div>
 
                                     <div className="footer-menu">
-
-
-                                        <div><p><span>JULY 7, 2018</span><br/>
-                                            Rowdy Can Walk!<br/>
-                                            <span>WATCH VIDEO</span></p>
-                                        </div>
+                                        {this.state.check?
+                                            <div className="fb-page" data-href="https://www.facebook.com/PegasusLaser/"
+                                                 data-tabs="timeline" data-width="500" data-height="300"
+                                                 data-small-header="false" data-adapt-container-width="true"
+                                                 data-hide-cover="false" data-show-facepile="true">
+                                                <blockquote cite="https://www.facebook.com/PegasusLaser/"
+                                                            className="fb-xfbml-parse-ignore"><a
+                                                    href="https://www.facebook.com/PegasusLaser/">Pegasus Therapy
+                                                    Laser</a></blockquote>
+                                            </div>:<div><p><span>JULY 7, 2018</span><br/>
+                                                Rowdy Can Walk!<br/>
+                                                <span>WATCH VIDEO</span></p>
+                                            </div>}
                                     </div>
-
                                 </div>
                                 <div className="col-sm-2 col-2 social-icons mt-5">
-                                    <a href="#"><i className="fa fa-linkedin-square redcolor socialicons socialicons1" ></i></a>
-                                    <a href="#"><i className="fa fa-twitter redcolor socialicons socialicons1"></i></a>
-                                    <a href="#"><i className="fa fa-envelope redcolor  socialicons socialicons1" ></i></a>
+                                    <Link href="https://www.linkedin.com/company/litecure-llc"><a><i className="fa fa-linkedin-square redcolor socialicons socialicons1" /></a></Link>
+                                    <Link href="https://twitter.com/PegasusLaser"><a><i className="fa fa-twitter redcolor socialicons socialicons1"/></a></Link>
+                                    <Link href="mailto:customer.service@litecure.com"><a><i className="fa fa-envelope redcolor  socialicons socialicons1" /></a></Link>
+                                    <Link href="https://www.instagram.com/pegasustherapy/"><a><i className="fa fa-instagram socialicons redcolor  socialicons1" /></a></Link>
                                 </div>
                             </div>
 
@@ -66,7 +121,6 @@ class Footer extends Component{
 
                     </div>
                 </div>
-
             </footer>
         )
     }
--- pages/products.js
+++ pages/products.js
@@ -4,13 +4,18 @@ import Layout from "../components/layout";
 import { withRouter } from "next/router";
 import Products from '../components/product'
 class Product extends Component {
-
+    state={
+        slug:null
+    };
+    componentWillMount() {
+        this.setState({ slug:"slug" })
+    };
 
     render() {
         return (
             <Layout type={"product"}>
                 <Products/>
-            </Layout>
+            </Layout >
         )
     }
 }
