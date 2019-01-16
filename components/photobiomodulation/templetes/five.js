901ae72b22e27616860bbe5eaf0157cd16ff6133
saadalishah <socialsolution15@gmail.com>
2019-01-16 21:39:04 +0500

    updated

--- components/photobiomodulation/templetes/five.js
+++ components/photobiomodulation/templetes/five.js
@@ -12,11 +12,14 @@ class TempleteFive extends Component {
                         the beam area and the PBM treatment is delivered by scanning the treatment head over the
                         treatment area in a continuous movement. Treatment with Companion lasers is non-invasive and it
                         is not possible to measure the dose delivered to the target tissue; it is only possible to
-                        measure the dose delivered to the treatment area.</p>
+                        measure the <a href="http://www.litecure.com/about-photobiomodulation/photobiomodulation-glossary/">dose delivered to the treatment area.</a></p>
                     <p>
                         However, we do know from cadaver, animal studies, and modeling that the absorption of the laser
                         in the skin and fat layers on its way to the target tissue can significantly reduce the dose
-                        that gets to the target tissue. The most prevalent method of indicating laser therapy dosage is
+                        that gets to the target tissue. The most prevalent method of indicating
+                        <a href="http://www.litecure.com/about-photobiomodulation/photobiomodulation-glossary/">
+                        laser therapy dosage
+                        </a> is
                         to measure the density of energy applied to the tissue surface. This is typically expressed in
                         J/cm2. Some variation in clinical effects can be observed; particularly at very high (>50W) or
                         very low (&lt;1 W) power settings using the same J/cm2 dose. The measurement technique works well for
