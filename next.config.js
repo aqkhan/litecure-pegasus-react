574dd406465d0031db92c94ed26db132ddba8560
akashiintellect <akash.iintellect@gmail.com>
2019-02-02 20:11:57 +0500

    new next config setup

--- next.config.js
+++ next.config.js
@@ -1,30 +1,14 @@
 const withCSS = require('@zeit/next-css')
 /* Without CSS Modules, with PostCSS */
 module.exports = withCSS();
-
-// const getRoutes = require('./routes');
-//
 // module.exports = {
-//     exportPathMap: getRoutes
-// };
-
-/* With CSS Modules */
-// module.exports = withCSS({ cssModules: true })
-
-/* With additional configuration on top of CSS Modules */
-
-// module.exports = withCSS({
-//   cssModules: true,
-//   webpack: function (config) {
-//     return config;
-//   }
-// });
-
-
-// module.exports = {
-//     exportPathMap: function () {
-//         return {
-//             '/': { page: '/' }
-//         }
+//   webpack: config => {
+//     // Fixes npm packages that depend on `fs` module
+//     config.node = {
+//       fs: 'empty'
 //     }
+
+//     return config
+//   }
 // }
+
--- server.js
+++ server.js
@@ -1,109 +1,65 @@
-const express = require('express');
-const next = require('next');
-const { parse } = require('url');
+const express = require("express");
+const next = require("next");
 
 const dev = process.env.NODE_ENV !== 'production';
-const PORT = process.env.PORT || 3000;
-
-const app = next({dir: '.', dev });
+const app = next({ dev });
 const handle = app.getRequestHandler();
 
-const getRoutes = require('./routes');
+app.prepare()
+    .then(() => {
+        const server = express();
 
-const routes = getRoutes();
-app.prepare().then(() => {
-    const server = express();
+        server.get('/product/:title', (req, res) => {
+            const actualPage = "/product";
+            const queryParams = {title: req.params.title};
+            app.render(req, res, actualPage, queryParams);
+        });
+        server.get('/advisory-board-member-detail/:name', (req, res) => {
+            const actualPage = "/advisory-board-member-detail";
+            const queryParams = {name: req.params.name};
+            app.render(req, res, actualPage, queryParams);
+        });
+        server.get('/published-paper-detail/:name', (req, res) => {
+            const actualPage = "/published-paper-detail";
+            const queryParams = {name: req.params.name};
+            app.render(req, res, actualPage, queryParams);
+        });
+        server.get('/casestudy-detail/:name', (req, res) => {
+            const actualPage = "/casestudy-detail";
+            const queryParams = {name: req.params.name};
+            app.render(req, res, actualPage, queryParams);
+        });
+        server.get('/article-detail/:name', (req, res) => {
+            const actualPage = "/article-detail";
+            const queryParams = {name: req.params.name};
+            app.render(req, res, actualPage, queryParams);
+        });
+        server.get('/post/:name', (req, res) => {
+            const actualPage = "/post";
+            const queryParams = {name: req.params.name};
+            app.render(req, res, actualPage, queryParams);
+        });
+        server.get('/webinar/:name', (req, res) => {
+            const actualPage = "/webinar";
+            const queryParams = {name: req.params.name};
+            app.render(req, res, actualPage, queryParams);
+        });
+
+        server.get('/detail/:name', (req, res) => {
+            const actualPage = "/detail";
+            const queryParams = {name: req.params.name};
+            app.render(req, res, actualPage, queryParams);
+        });
 
 
+        server.get('*', (req, res) => {
+            return handle(req, res)
+        });
 
-    server.get('/product/:title', (req, res) => {
-        const actualPage = "/product";
-        const queryParams = {title: req.params.title};
-        app.render(req, res, actualPage, queryParams);
+        server.listen(3000, (err) => {
+            if (err) throw err;
+        })
+    })
+    .catch((ex) => {
+        process.exit(1)
     });
-    server.get('/advisory-board-member-detail/:name', (req, res) => {
-        const actualPage = "/advisory-board-member-detail";
-        const queryParams = {name: req.params.name};
-        app.render(req, res, actualPage, queryParams);
-    });
-    server.get('/published-paper-detail/:name', (req, res) => {
-        const actualPage = "/published-paper-detail";
-        const queryParams = {name: req.params.name};
-        app.render(req, res, actualPage, queryParams);
-    });
-    server.get('/casestudy-detail/:name', (req, res) => {
-        const actualPage = "/casestudy-detail";
-        const queryParams = {name: req.params.name};
-        app.render(req, res, actualPage, queryParams);
-    });
-    server.get('/article-detail/:name', (req, res) => {
-        const actualPage = "/article-detail";
-        const queryParams = {name: req.params.name};
-        app.render(req, res, actualPage, queryParams);
-    });
-    server.get('/post/:name', (req, res) => {
-        const actualPage = "/post";
-        const queryParams = {name: req.params.name};
-        app.render(req, res, actualPage, queryParams);
-    });
-    server.get('/webinar/:name', (req, res) => {
-        const actualPage = "/webinar";
-        const queryParams = {name: req.params.name};
-        app.render(req, res, actualPage, queryParams);
-    });
-
-    server.get('/detail/:name', (req, res) => {
-        const actualPage = "/detail";
-        const queryParams = {name: req.params.name};
-        app.render(req, res, actualPage, queryParams);
-    });
-
-
-
-    server.get('*', (req, res) => {
-        const parsedUrl = parse(req.url, true);
-        const { pathname, query = {} } = parsedUrl;
-        const route = routes[pathname];
-        if (route) {
-            return app.render(req, res, route.page, query);
-        }
-        return handle(req, res, 'index');
-    });
-
-
-
-
-    server.listen(PORT, (err) => {
-        if (err) throw err;
-        console.log(`> Ready on http://localhost:${PORT}`);
-    });
-})
-
-// const express = require("express");
-// const next = require("next");
-// const dev = process.env.NODE_ENV !== 'production';
-// const app = next({ dev });
-// const handle = app.getRequestHandler();
-//
-// app.prepare()
-//     .then(() => {
-//         const server = express();
-//         server.get('/product/:id', (req, res) => {
-//             const actualPage = "/products";
-//             const queryParams = {name: req.params.id};
-//             app.render(req, res, actualPage, queryParams);
-//         });
-//
-//
-//
-//         server.get('*', (req, res) => {
-//             return handle(req, res)
-//         });
-//
-//         server.listen(3000, (err) => {
-//             if (err) throw err;
-//         })
-//     })
-//     .catch((ex) => {
-//         process.exit(1)
-//     });
