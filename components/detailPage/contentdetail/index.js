ed2c06b4df8a217f9499a386544376a673a9c5f5
akashiintellect <akash.iintellect@gmail.com>
2019-02-07 20:12:29 +0500

    embed bug fixed

--- components/detailPage/contentdetail/index.js
+++ components/detailPage/contentdetail/index.js
@@ -1,12 +1,10 @@
-/**
- * Created by FaZi on 12/27/2018.
- */
 import React, {Component} from 'react';
 import embed from 'embed-video';
+
 class DetailContent extends Component {
     render(){
-        console.log("video " + this.props.embedVideo)
-        return <div>
+        console.log("video " + embed(this.props.embedVideo))
+        return (<div>
             <section className="employees">
                 <div className="container">
                     <div className="detail-emp">
@@ -15,12 +13,9 @@ class DetailContent extends Component {
                     </div>
                 </div>
             </section>
-            <div className="container-fluid benefit-video-con">
-            {       
-                embed(this.props.embedVideo, {attr:{width:"100%", height:500}})
-                }
-            </div>
-        </div>
+            {this.props.embedVideo && <div className="container-fluid benefit-video-con" dangerouslySetInnerHTML={{__html:embed(this.props.embedVideo,{ attr:{width:"100%", height:500}})}}>      
+                 </div>}
+        </div>)
     }
 }
 export default DetailContent;
